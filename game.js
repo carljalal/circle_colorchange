da8ace60845d6dcef01df3378fcbe491cc0046d9
Carl Jalal <carljalal@gmail.com>
2020-07-23 18:13:21 -0400

    added highest score, formatted timer

--- game.js
+++ game.js
@@ -35,7 +35,7 @@ last_color_pick = 0;
 let number_completed = -1;
 const timer_max = 2000
 time_left = timer_max;
-
+let highest_score = 0;
 
 function pick_color() {
     if (new_pick == true) {
@@ -54,13 +54,13 @@ function pick_color() {
     else if (color_pick == 3) this.color = 'teal';
     else if (color_pick == 4) this.color = 'orange';
     else if (color_pick == 5) this.color = 'purple';
-    a_circle(0.5, 0.4, 0.35, color);
+    a_circle(0.5, 0.43, 0.35, color);
     //writeMessage()
 }
 
 function gameLoop() {
     window.requestAnimationFrame(gameLoop);
-    
+    display_highscore();
 
     if (first_draw == true) {
         number_completed = -1;
@@ -73,8 +73,9 @@ function gameLoop() {
         first_draw = false;
 
         pick_color();
-        writeMessage(current_message, 'black', cw*0.5);
-        writeMessage('(Press Left or Right)','black', cw *0.5, y = ch*0.45, '16pt' )
+        writeMessage(current_message, 'black', cw * 0.5);
+        writeMessage('(Press Left or Right)', 'black', cw * 0.5, y = ch * 0.45, '16pt')
+
     }
 
     currentTime = Date.now()
@@ -85,7 +86,13 @@ function gameLoop() {
 
 }
 
-writeMessage = (message, col = 'black', x = cw*0.49, y = ch*0.4, fontsize='20pt') => {
+display_highscore = () => {
+    cx.clearRect(cw * 0.66, 5, cw * 0.33, ch * 0.04);
+    cx.strokeRect(cw * 0.66, 5, cw * 0.33, ch * 0.04);
+    writeMessage('Highest Score:    ' + highest_score, 'black', cw * 0.8, 20, '12pt')
+}
+
+writeMessage = (message, col = 'black', x = cw * 0.49, y = ch * 0.4, fontsize = '20pt') => {
     //let context = canvas.getContext('2d');
     //context.clearRect(0, 0, canvas.width, canvas.height);
     cx.font = fontsize + ' Calibri';
@@ -94,26 +101,25 @@ writeMessage = (message, col = 'black', x = cw*0.49, y = ch*0.4, fontsize='20pt'
     cx.fillText(message, x, y);
 }
 
-function is_correct(){
+function is_correct() {
+    current_message = 'CORRECT';
     number_completed++;
-    if(number_completed < timer_max * 0.2){
+    if (number_completed < timer_max * 0.2) {
         time_left = time_left + timer_max - number_completed;
     }
     else {
         time_left = time_left + timer_max * 0.2;
     }
-    if(time_left > timer_max) time_left = timer_max;
+    if (time_left > timer_max) time_left = timer_max;
 }
 
-function is_left(){
+function is_left() {
     if (number_completed > -1) {
         if (last_color_pick != color_pick) {
-            current_message = 'CORRECT';
             is_correct();
         }
         else {
-            current_message = startmessage;
-            first_draw = true;
+            is_wrong();
         }
     }
     else {
@@ -122,15 +128,19 @@ function is_left(){
     before_changing_color();
 }
 
-function is_right(){
+is_wrong = () => {
+    current_message = startmessage;
+    if (number_completed > highest_score) highest_score = number_completed;
+    first_draw = true;
+}
+
+function is_right() {
     if (number_completed > -1) {
         if (last_color_pick == color_pick) {
-            current_message = 'CORRECT';
             is_correct();
         }
         else {
-            current_message = startmessage;
-            first_draw = true;
+            is_wrong();
         }
 
     }
@@ -159,7 +169,7 @@ canvas.addEventListener('touchstart', e => {
     let rect = canvas.getBoundingClientRect();
     startx = e.targetTouches[0].pageX - rect.left;
     starty = e.targetTouches[0].pageY - rect.top;
-    touched_button(startx,starty);
+    touched_button(startx, starty);
 })
 
 /*
@@ -170,46 +180,47 @@ canvas.addEventListener('mousedown', e => {
     touched_button(startx,starty);
 })
 */
-function touched_button(startx,starty){
-    if(startx < 250 && starty > 400){
+function touched_button(startx, starty) {
+    if (startx < 250 && starty > 400) {
         is_left();
     }
-    if(startx > 250 && starty > 400){
+    if (startx > 250 && starty > 400) {
         is_right();
     }
 }
 
-function draw_timer(){
-    if(number_completed > -1){
+function draw_timer() {
+    if (number_completed > -1) {
         time_left = time_left - delta;
-        cx.clearRect(5,5,200,20);
-        cx.fillStyle = 'lime';
-        cx.fillRect(5,5,200*(time_left/timer_max),20);
-        cx.strokeRect(5,5,200,20);
-        writeMessage('timer','grey',80,20,'15pt');
-        if(time_left <= 0) {
-            first_draw = true;
-        }
+    }
+
+    cx.clearRect(5, 5, 300, 20);
+    cx.fillStyle = 'lime';
+    cx.fillRect(5, 5, 300 * (time_left / timer_max), 20);
+    cx.strokeRect(5, 5, 300, 20);
+    writeMessage('timer', 'grey', 150, 20, '12pt');
+    if (time_left <= 0) {
+        first_draw = true;
     }
 }
 
-function draw_buttons(){
+function draw_buttons() {
     cx.clearRect(0, 0, cw, cw);
     cx.fillStyle = 'black'
-    cx.fillRect(260,410,250,90)
+    cx.fillRect(260, 410, 250, 90)
     cx.fillStyle = 'black'
-    cx.fillRect(0,410,240,90)
-    writeMessage('left','white',100,450)
-    writeMessage('right','white',330,450)
+    cx.fillRect(0, 410, 240, 90)
+    writeMessage('left', 'white', 115, 460)
+    writeMessage('right', 'white', 380, 460)
 }
 
 function before_changing_color() {
     new_pick = true;
     draw_buttons();
     pick_color();
-    if (number_completed > -1) writeMessage(number_completed);
+    if (number_completed > -1) writeMessage(number_completed, 'black', cw * 0.49, ch * 0.43);
     else writeMessage(current_message)
-    
+
 }
 
 gameLoop()
